e88c2763f4e74753b1b46da239d6bd0625e40390
César Zárate <cesarjzo@outlook.com>
2023-05-17 21:00:22 -0600

    Add documentation

--- IntermediateCode/IcvExecutable.cs
+++ IntermediateCode/IcvExecutable.cs
@@ -16,8 +16,14 @@ public class IcvExecutable
     private readonly Func<string> _readFunction;
     private readonly Action<string> _writeFunction;
 
-    public IcvExecutable(IntermediateCodeVector vector, Symbol[] symbols, Func<string> readFunction,
-        Action<string> writeFunction)
+    /// <summary>
+    /// Creates a new instance of <see cref="IcvExecutable"/>.
+    /// </summary>
+    /// <param name="vector">Intermediate code vector to be executed.</param>
+    /// <param name="symbols">Symbols table from semantic analysis.</param>
+    /// <param name="readFunction">Predicate to be used as read function.</param>
+    /// <param name="writeFunction">Action to be used as write function.</param>
+    public IcvExecutable(IntermediateCodeVector vector, Symbol[] symbols, Func<string> readFunction, Action<string> writeFunction)
     {
         _vector = vector;
         _symbols = symbols;
@@ -26,6 +32,10 @@ public class IcvExecutable
         _executionStack = new Stack<Token>();
     }
 
+    /// <summary>
+    /// Executes the intermediate code vector.
+    /// </summary>
+    /// <returns>The symbols table with the updated values.</returns>
     public Symbol[] ExecuteIcv()
     {
         for (var i = 0; i < _vector.Count(); i++)
@@ -62,6 +72,10 @@ public class IcvExecutable
         return _symbols;
     }
 
+    /// <summary>
+    /// Assigns the result of the input function to the next identifier token in vector.
+    /// </summary>
+    /// <param name="index">The index of the current token.</param>
     private void AssignToNext(int index)
     {
         Token nextToken = _vector[index + 1];
@@ -70,6 +84,10 @@ public class IcvExecutable
         symbol.Value = _readFunction();
     }
 
+    /// <summary>
+    /// Writes the value of the next token of icv to the output functions specified in the constructor.
+    /// </summary>
+    /// <param name="index">The index of the current token.</param>
     private void WriteNext(int index)
     {
         Token nextToken = _vector[index + 1];
@@ -78,6 +96,11 @@ public class IcvExecutable
         _writeFunction(symbol.Value);
     }
 
+    /// <summary>
+    /// Assigns the right operand's value to the left operand's identifier.
+    /// </summary>
+    /// <param name="leftOperand">Should be an Identifier token.</param>
+    /// <param name="rightOperand">Identifier or Literal token.</param>
     private void AssignRightToLeft(Token leftOperand, Token rightOperand)
     {
         string rightValue = Lang.IsIdentifier(rightOperand)
@@ -88,6 +111,10 @@ public class IcvExecutable
         symbolOfLeftOperand.Value = rightValue;
     }
 
+    /// <summary>
+    /// Pops the last two operands from the stack.
+    /// </summary>
+    /// <returns>Returns a tuple of the two operands (left, right).</returns>
     private (Token, Token) PopOperands()
     {
         Token rightOperand = _executionStack.Pop();
@@ -96,6 +123,13 @@ public class IcvExecutable
         return (leftOperand, rightOperand);
     }
 
+    /// <summary>
+    /// Evaluates the operation and returns the result.
+    /// </summary>
+    /// <param name="leftOperand">If operand is an identifier, then it's value is taken from the symbol table. Otherwise, the lexeme is taken.</param>
+    /// <param name="rightOperand">If operand is an identifier, then it's value is taken from the symbol table. Otherwise, the lexeme is taken.</param>
+    /// <param name="op">The operator to be evaluated.</param>
+    /// <returns>The result of the operation.</returns>
     private object Evaluate(Token leftOperand, Token rightOperand, Token op)
     {
         string leftValue = Lang.IsIdentifier(leftOperand)
